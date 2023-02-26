2705f826a78f1667c4a647023dd1cf22758e03db
John-Peter Eberhard <johnpetereberhard@gmail.com>
2023-02-26 16:21:06 -0700

    SET UP

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
+  title = 'Tour of Heros';
+ 
 }
