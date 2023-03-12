b51c46f998d86b5ce0f00ceb0361be8d3c9333b5
John-Peter Eberhard <johnpetereberhard@gmail.com>
2023-03-12 04:35:54 -0600

    nav stuff added

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { Hero } from '../heros'
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
+import { HeroService } from 'src/hero.service';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
@@ -8,4 +11,22 @@ import { Hero } from '../heros'
 })
 export class HeroDetailComponent {
   @Input() hero?: Hero;
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+  ngOnInit(): void {
+    this.getHero();
+  }
+  
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    
+    this.heroService.getHero(id)
+    .subscribe(heroes => this.hero = heroes);
+  }
+  goBack(): void {
+    this.location.back();
+  }
 }
